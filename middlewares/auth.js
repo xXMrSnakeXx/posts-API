a62af755eb9db783b599cef0e1e8248db92b1a4e
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2023-11-21 11:58:32 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -10,13 +10,15 @@ const auth = async (req, res, next) => {
   const { authorization = "" } = req.headers;
 
   const [bearer, token] = authorization.split(" ");
-  console.log(token);
+
   if (bearer !== "Bearer") {
     next(createError(401, "Not authorized"));
   }
   try {
-    const { id } = jwt.verify(token, SECRET_KEY);
-    const user = await User.findOne({ id });
+    const { email } = jwt.verify(token, SECRET_KEY);
+
+    const user = await User.findOne(email);
+
     if (!user || !user.token) {
       next(createError(401, "Not authorized"));
     }
